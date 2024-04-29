4563aa917d25edfc2daa759445970b084261f3b1
Yuan Si <do4suki@gmail.com>
2024-04-29 21:17:07 +0800

    Disable unused plugins
    Signed-off-by: Yuan Si <do4suki@gmail.com>

--- main.go
+++ main.go
@@ -80,14 +80,14 @@ import (
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/chrev"            // 英文字符翻转
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/coser"            // 三次元小姐姐
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/cpstory"          // cp短打
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/dailynews" // 今日早报
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/dailynews" // 今日早报
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/danbooru"         // DeepDanbooru二次元图标签识别
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/diana"            // 嘉心糖发病
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/dish"             // 程序员做饭指南
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/drawlots"         // 多功能抽签
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/dress"            // 女装
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/driftbottle"      // 漂流瓶
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/emojimix" // 合成emoji
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/emojimix" // 合成emoji
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/event"            // 好友申请群聊邀请事件处理
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/font"             // 渲染任意文字到图片
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/fortune"          // 运势
